846c14c0e175e564aceb1fab2b760c66a844e259
bbangHo <sonsins1@naver.com>
2026-09-17 15:56:16 +0900

    refactor: [solution-08] MemberService Facade와 UseCase로 분리

--- src/main/java/com/back/member/app/MemberJoinUsecase.java
+++ src/main/java/com/back/member/app/MemberJoinUsecase.java
@@ -11,26 +11,12 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class MemberJoinUseCase {
+public class MemberJoinUsecase {
     private final MemberRepository memberRepository;
-
-    public long count() {
-        return memberRepository.count();
-    }
-
-    @Transactional(readOnly = true)
-    public Optional<Member> findByUsername(String username) {
-        return memberRepository.findByUsername(username);
-    }
-
-    @Transactional(readOnly = true)
-    public Optional<Member> findById(Integer id) {
-        return memberRepository.findById(id);
-    }
 
     @Transactional
     public Member join(String username, String password, String nickname) {
-        findByUsername(username).ifPresent(m -> {
+        memberRepository.findByUsername(username).ifPresent(m -> {
             throw new DomainException("409-1", "이미 존재하는 username 입니다.");
         });
 
