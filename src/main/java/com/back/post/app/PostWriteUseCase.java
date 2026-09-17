6c1100cfd070d7b0edfbd8515dfe97fd40836eea
bbangHo <sonsins1@naver.com>
2026-09-17 17:48:26 +0900

    feat: [solution-13] 글 작성시, 회원에 관련된 랜덤 팁을 제공해야 한다.

--- src/main/java/com/back/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/post/app/PostWriteUseCase.java
@@ -2,7 +2,11 @@ package com.back.post.app;
 
 import com.back.global.dto.PostDto;
 import com.back.global.event.PostCreateEvent;
+import com.back.global.response.RsData;
+import com.back.member.app.MemberFacade;
 import com.back.member.domain.Member;
+import com.back.member.in.MemberEventListener;
+import com.back.member.out.MemberApiClient;
 import com.back.post.domain.Post;
 import com.back.post.out.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,21 +14,27 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @RequiredArgsConstructor
 @Service
 public class PostWriteUseCase {
     private final PostRepository postRepository;
+    private final MemberFacade memberFacade;
     private final ApplicationEventPublisher publisher;
 
     @Transactional
-    public Post write(Member author, String title, String content) {
-        Post post = new Post(author, title, content);
+    public RsData<Post> write(Member author, String title, String content) {
+        Post post = postRepository.save(new Post(author, title, content));
 
         // 게시글 작성시 활동점수 3점 추가
         publisher.publishEvent(new PostCreateEvent(new PostDto(post)));
 
-        return postRepository.save(post);
+        String randomSecureTip = memberFacade.getRandomSecureTip();
+
+        return new RsData<>(
+                "201-1",
+                "%d번 글이 생성되었습니다. 보안 팁 : %s"
+                        .formatted(post.getId(), randomSecureTip),
+                post
+        );
     }
 }
