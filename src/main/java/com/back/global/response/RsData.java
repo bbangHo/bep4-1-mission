f3daa1b50762d00d79b788625da336f31c783afe
bbangHo <sonsins1@naver.com>
2026-09-22 14:08:38 +0900

    refactor: 인터페이스 도입하여 규격 만들기

--- src/main/java/com/back/global/response/RsData.java
+++ src/main/java/com/back/global/response/RsData.java
@@ -1,21 +1,29 @@
 package com.back.global.response;
 
+import com.back.standard.ResultType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
-public class RsData<T> {
+public class RsData<T> implements ResultType {
     public String code;
     public String message;
     public T data;
 
-    public RsData (String code, String message, T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
-
     public RsData (String code, String message) {
         this.code = code;
         this.message = message;
+        data = null;
+    }
+
+    @Override
+    public String getResultCode() {
+        return code;
+    }
+
+    @Override
+    public String getMsg() {
+        return message;
     }
 }
